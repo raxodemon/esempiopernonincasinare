a0bcc683d3edeaa6eb0d8447ed61e2e8047c8f49
David Rajo <davidraxo@gmail.com>
2023-09-14 14:58:15 +0200

    no message

--- src/test/java/it/csi/serse/sersesrvqrk/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package it.csi.serse.sersesrvqrk;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
